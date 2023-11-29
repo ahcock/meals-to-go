9f53a9cbc0b4cfdda578ebcfe032099c2d4f0c3c
ahcock <srparkgogo@gmail.com>
2023-11-29 23:36:16 +0900

    chore: 보일러플레이트 관련 코드 제거 및 앱 버전명 변경

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>나왔다!!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      빈 화면
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
